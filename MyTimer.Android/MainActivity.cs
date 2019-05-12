c7f907d43a6fd73157ea12ce3b4d0ba9bdab6417
nobe1234 <sohei.nobe@rakus.co.jp>
2019-05-12 15:11:08 +0900

    ImageResourceExtensionクラスの実装 リソースから指定したパスの画像を読み込む。

--- MyTimer.Android/MainActivity.cs
+++ MyTimer.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MyTimer.Droid
 {
-    [Activity(Label = "MyTimer", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MyTimer", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ MyTimer/ImageResourceExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace MyTimer
+{
+    // 共通プロジェクトのリソース画像を表示のためのマークアップ拡張
+    [ContentProperty("Source")]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        // リソースファイルのパスを取得または設定する
+        public string Source { get; set; }
+
+        // Sourceで指定されたリソース情報を取得する。
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+            // リソースからパスの情報読み込む
+            var imageSource = ImageSource.FromResource(Source);
+
+            return imageSource;
+        }
+    }
+}
